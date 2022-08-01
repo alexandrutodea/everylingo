90995d98ef5d85dab938c55850784c39f59d3b39
alexandrutodea <alexandru.todea01@e-uvt.ro>
2022-08-01 17:59:27 +0300

    Initial Commit

---
+++ src/main/java/com/everylingo/everylingoapp/EverylingoAppApplication.java
@@ -0,0 +1,13 @@
+package com.everylingo.everylingoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverylingoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverylingoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everylingo/everylingoapp/EverylingoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everylingo.everylingoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverylingoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
