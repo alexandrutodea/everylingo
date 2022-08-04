a04fd5b72a34702045d4563e812485d919069b56
alexandrutodea <alexandru.todea01@e-uvt.ro>
2022-08-04 11:25:43 +0300

    Made some formatting changes to WebSecurityConfig.java and index.html

--- src/main/java/com/everylingo/everylingoapp/security/WebSecurityConfig.java
+++ src/main/java/com/everylingo/everylingoapp/security/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -22,12 +21,11 @@ public class WebSecurityConfig {
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .logout(l -> l
-                        .logoutSuccessUrl("/")
-                        .permitAll()
-                )
                 .csrf(c -> c
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
                 .oauth2Login();
 
         return http.build();
